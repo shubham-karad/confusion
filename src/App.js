821ed70253e5d41e3767ca43bab11dfda336a68e
shubham-karad <shubham4986@gmail.com>
2020-11-23 11:29:08 +0530

    components part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import Menu from './components/MenuComponent';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
-    <div >
-    
-    </div>
+    <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
   );
 }
 
